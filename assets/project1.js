f9af1a34d172c0aa504ab3d361ccec2db9fffc4f
DWatson <mdwat90@gmail.com>
2018-09-06 20:34:40 -0600

    Updates

---
+++ assets/project1.js
@@ -0,0 +1,25 @@
+// $(document).ready(function() {
+//     console.log("ready!");
+
+//     var eventType = $("#event").val().trim();
+//     var eventType = $("#event").val().trim();
+    
+
+//     var queryURL = "https://api.eventful.com/&pp_key=brt8hcnCmfWwn2qw&q=" + eventType;
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//         // data: {
+//         //     q: eventType,
+//         //     app_key:"brt8hcnCmfWwn2qw",
+//         // }
+//     }).then(function (response) {
+//         console.log(response);
+//         // $('#card1title').text(response.q);
+//     });
+
+//     // Google Maps API Key: "AIzaSyDp-fxPQKMid_VxTXoUzWGvf_i3INPPNnM"
+
+
+// });
