bda10078da62e2acf8a67f2da7dfe8f290d6cef1
DWatson <mdwat90@gmail.com>
2018-09-08 11:39:08 -0600

    Updates

--- assets/project1.js
+++ assets/project1.js
@@ -1,16 +1,16 @@
-function() {
-    $("#map").googleMap({
-      zoom: 10, // Initial zoom level (optional)
-      coords: [48.895651, 2.290569], // Map center (optional)
-      type: "ROADMAP" // Map type (optional)
-    });
-  })
+var map;
 
-function() {
-    $("#map").googleMap();
-    $("#map").addMarker({
-      coords: [48.895651, 2.290569], // GPS coords
-      url: 'http://www.tiloweb.com', // Link to redirect onclick (optional)
-      id: 'marker1' // Unique ID for your marker
-    });
-  })
+function initMap() {
+  var Denver = {lat: 39.742043, lng: -104.991531};
+  var event1 = {lat: 39.802765, lng: -105.087486};
+  var event2 = {lat: 39.710850, lng: -105.081505};
+  var event3 = {lat: 39.649055, lng: -104.976425};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: Denver,
+    zoom: 8
+  });
+  // var marker = new google.maps.Marker({position: Denver, map: map});
+  var marker = new google.maps.Marker({position: event1, map: map});
+  var marker = new google.maps.Marker({position: event2, map: map});
+  var marker = new google.maps.Marker({position: event3, map: map});
+}
